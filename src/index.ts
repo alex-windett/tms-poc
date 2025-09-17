6d19f4df73fd09313167df1dcbd60644825f729c
Alex Windett <windettalex@gmail.com>
2025-09-17 12:31:40 +0100

    add toglee route

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+import githubRoute from '../routes/tolgee/github.ts';
+
+const app = express();
 
 const welcomeStrings = [
-  "Hello Express!",
-  "To learn more about Express on Vercel, visit https://vercel.com/docs/frameworks/backend/express",
-]
+	'Hello Express!',
+	'To learn more about Express on Vercel, visit https://vercel.com/docs/frameworks/backend/express',
+];
 
 app.get('/', (_req, res) => {
-  res.send(welcomeStrings.join('\n\n'))
-})
+	res.send(welcomeStrings.join('\n\n'));
+});
 
-export default app
+app.post('/tolgee/github', githubRoute);
+
+app.listen(3001, function () {
+	console.log('Example app listening on port ' + 3001 + '!');
+});
+
+export default app;
